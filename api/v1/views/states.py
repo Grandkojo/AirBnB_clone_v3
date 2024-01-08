7f0327cc6a82d63046f16d3dd3bbbc68f7ee257d
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-01-08 06:01:47 -0800

    v3

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,14 +8,14 @@ from models.state import State
 from api.v1.views import app_views
 
 
-@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states', strict_slashes=False)
 def get_all_states():
     """Retrieves the list of all State objects"""
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', strict_slashes=False)
 def get_state(state_id):
     """Retrieves a State object by id"""
     state = storage.get(State, state_id)
